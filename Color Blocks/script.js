8dee9990a86b6ba10d57864fc1c86436e33bc2b3
Bharath-k06 <62843177+Bharath-k06@users.noreply.github.com>
2021-04-26 08:43:57 +0530

    Add files via upload

---
+++ Color Blocks/script.js
@@ -0,0 +1,13 @@
+function func1(){
+    console.log("Entered the area1");
+}
+
+function func2(){
+    console.log("Entered the area2");
+}
+function func3(){
+    console.log("Entered the area3");
+}
+function func4(){
+    console.log("Dead");
+}
